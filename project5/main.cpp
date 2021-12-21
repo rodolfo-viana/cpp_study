9cc406c24e16464e50e27cbb413150fafcf4e6c4
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2021-12-21 18:37:37 -0300

    Added lesson 5

---
+++ project5/main.cpp
@@ -0,0 +1,33 @@
+// cout, cin, cerr, clog
+
+/* They are objects representing streams
+ * 
+ * cout: standard output stream; console
+ * - examples: 
+ *      cout << data; -> to show data var value onto console
+ *      cout << "data is " << data;
+ *      cout << "data is " << data << endl; -> to insert newline
+ *      cout << "data is " << data << "\n"; -> to insert newline
+ * 
+ * cin: standard input stream; keyboard, buffer
+ * - examples:
+ *      cin >> data; -> to store keyboard value into data var
+ *      cin >> data1 >> data2; -> to store multiple values into multiple vars
+ * 
+ * <<: insertion operator; output streams
+ * >>: extraction operator; input streams */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number1;
+    int number2;
+    
+    cout << "Type two numbers separated with a space: ";
+    cin >> number1 >> number2;
+    cout << "Ok. I assigned your first number to variable 'number1'. It is " << number1 << "." << endl;
+    cout << "And your second number is stored as 'number2'. It is " << number2 << "." << endl;
+    return 0;
+}
