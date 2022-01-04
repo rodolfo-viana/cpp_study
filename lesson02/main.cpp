7bea83297cee0ceb9ba63bee7393725bf01f59a0
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-01-04 08:23:45 -0300

    Moved projects 1, 2 to the same workspace; renamed folders

--- lesson02/main.cpp
+++ lesson02/main.cpp
@@ -1,10 +1,10 @@
 /* Challenge
- * 
+ *
  * Create a C++ program that asks the user
  * for his favorite number between 1 and 100,
  * then reads this number from the console.
  */
- 
+
 #include <iostream>
 
 int main(){
@@ -13,4 +13,4 @@ int main(){
     std::cin >> fav_number;
     std::cout << "Amazing! Did you know that " << fav_number << " is my favorite number too?" << std::endl;
     return 0;
-} 
+}
