cd9e397e565e34f3921a1809bc5c8d7b256ae66d
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-01-15 09:03:16 -0300

    Added lesson 21: comparison

---
+++ lesson21/main.cpp
@@ -0,0 +1,23 @@
+// Comparison operators
+
+/* They are:
+ * - >
+ * - >=
+ * - <
+ * - <=
+ * - <=> (C++20 only: if equal, 0; less than 0 on the left and greater on the right)
+ * */
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 {}, num2 {};
+    cout << boolalpha;
+    cout << "Enter two integers separated by space: ";
+    cin >> num1 >> num2;
+    cout << num1 << " > " << num2 << ": " << (num1 > num2) << endl;
+    cout << num1 << " >= " << num2 << ": " << (num1 >= num2) << endl;
+    cout << num1 << " < " << num2 << ": " << (num1 < num2) << endl;
+    cout << num1 << " <= " << num2 << ": " << (num1 <= num2) << endl;
+    return 0;
+}
