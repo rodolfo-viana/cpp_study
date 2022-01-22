b6eaa166e19e1f8cffa9d7b91c06fc224d5c056e
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2022-01-22 12:44:06 -0300

    Added lesson 23: compound assignment operators

---
+++ lesson23/main.cpp
@@ -0,0 +1,36 @@
+// Compound assignment operators
+
+/* The syntax is: op=
+ * Examples:
+ * - lhs += rhs; -> lhs = lhs + rhs;
+ * - lhs -= rhs; -> lhs = lhs - rhs;
+ * - lhs *= rhs; -> lhs = lhs * rhs;
+ * - lhs /= rhs; -> lhs = lhs / rhs;
+ * - lhs %= rhs; -> lhs = lhs % rhs;
+ * - lhs <<= rhs; -> lhs = lhs << rhs;
+ * - lhs >>= rhs; -> lhs = lhs >> rhs;
+ * Bitwise operations:
+ * - lhs &= rhs; -> lhs = lhs & rhs;
+ * - lhs ^= rhs; -> lhs = lhs ^ rhs;
+ * - lhs |= rhs; -> lhs = lhs | rhs;
+ * */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    double price {}, total {};
+    int qty {};
+
+    // First purchase
+    qty = 3;
+    price = 12.15;
+    total = price * qty; // 36.45
+    cout << total << endl;
+    // Second purchase
+    qty = 5;
+    price = 14.05;
+    total += (price * qty); // 36.45 + 70.25 = 106.7
+    cout << total << endl;
+    return 0;
+}
